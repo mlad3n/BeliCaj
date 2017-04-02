aa06238430d781bac78dc2d10f46ab7fec470aac
Marko Knežević <markok@nordeus.com>
2017-04-02 11:48:13 +0200

    run added

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python crawl.py https://www.reddit.com/r/blackhat/new/ https://www.reddit.com/r/security/new/ https://www.reddit.com/r/security/new/ https://www.reddit.com/r/darknet/
+python top
