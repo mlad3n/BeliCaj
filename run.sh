29e2aa9b939ed913e0167d076a6b3329d3e4b3b6
Marko Knežević <markok@nordeus.com>
2017-04-02 11:57:18 +0200

    run.sh updated

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 python crawl.py https://www.reddit.com/r/blackhat/new/ https://www.reddit.com/r/security/new/ https://www.reddit.com/r/security/new/ https://www.reddit.com/r/darknet/
 python topic_modeling_prep.py
+open Report.twb
